1b77f69daf40eb3b4b31fbc9323aa7a5e2a1edb0
lafranfabian <happinesschoices123@gmail.com>
2025-04-23 09:30:50 +0700

    react

---
+++ react/my-react-app/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+     <> 
+     {/* //jangan lupa ini (fragment) agar bisa ngereturn dan biar aman// */}
+    <h1>Hellow world </h1>
+    <h2>react with vite</h2>
+    
+    </>
+  )
+}
+
+export default App
